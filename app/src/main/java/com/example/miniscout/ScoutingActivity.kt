f54818a8cde1eead01422b796cfab426b19378f1
elliekim <ellie.yso.kim@gmail.com>
2021-06-17 20:08:57 -0700

    Part 6

--- app/src/main/java/com/example/miniscout/ScoutingActivity.kt
+++ app/src/main/java/com/example/miniscout/ScoutingActivity.kt
@@ -1,6 +1,26 @@
 package com.example.miniscout
 
 import android.app.Activity
+import android.os.Bundle
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.scouting_activity.*
 
 class ScoutingActivity : Activity() {
+    lateinit var match : Match
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scouting_activity)
+        retrieveMatchData()
+        initScoutingSetup()
+    }
+
+    private fun retrieveMatchData(){
+        match = Gson().fromJson(intent.extras!!.get(match_tag).toString(), Match::class.java)
+    }
+
+    private fun initScoutingSetup(){
+        tv_match_number.text = match.matchNumber
+        tv_team_number.text = match.teamNumber
+    }
 }
